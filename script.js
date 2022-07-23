05e38c199b873da9d27ac35942640b43e73fe638
Hjemlandet <62887571+Hjemlandet@users.noreply.github.com>
2022-07-23 19:44:45 +0300

    My perfect CV

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+// Geolocation  
+
+
+// Form 
+
+let sbm = document.querySelector('#sbm')
+
+sbm.textContent = sbm.textContent.toUpperCase() 
+
+//  testing 
+console.log(sbm.textContent) 
+
+alert('uraaaaaa')
